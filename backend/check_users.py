e029b039d16219474bcf430cbc08d823472a7aaf
Amanda <AC350144@gmail.com>
2025-09-21 19:53:17 -0700

    removing check_users.py

--- backend/check_users.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect("clipboard_history.db")
-c = conn.cursor()
-c.execute("SELECT username FROM users")
-users = c.fetchall()
-conn.close()
-print("Users in DB:", users)
